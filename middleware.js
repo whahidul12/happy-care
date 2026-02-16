324237968ab9e1143b4bea0e7a9c2c2aac1c0c9c
whahidul12 <mdislamwhahid@gmail.com>
2026-02-16 23:56:49 +0600

    remove local env file

--- middleware.js
+++ middleware.js
@@ -2,13 +2,12 @@ import { NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 
 export async function middleware(request) {
-    // Use NextAuth's getToken to check for valid session
-    const token = await getToken({ 
+
+    const token = await getToken({
         req: request,
-        secret: process.env.NEXTAUTH_SECRET 
+        secret: process.env.NEXTAUTH_SECRET
     });
 
-    // Define Private Routes based on requirements: Booking and My Bookings
     const isPrivateRoute =
         request.nextUrl.pathname.startsWith('/booking') ||
         request.nextUrl.pathname.startsWith('/my-bookings');
